da9595e9d1096657d938809b76350ad50754fb6c
carobte <caro.bustamante.escobar@gmail.com>
2024-10-21 06:28:39 -0500

    feat: basic web and laravel project base

---
+++ 02_basicweb/index.php
@@ -0,0 +1,18 @@
+<?php
+$nombre = "Caro"
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <h1>Hola $nombre</h1>
+
+    <script src="./js/main.js"></script>
+</body>
+</html>
